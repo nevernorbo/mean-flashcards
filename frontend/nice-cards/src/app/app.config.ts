4eace6113e2451dacd8041be5d9f3065fb0f92ca
nevernorbo <sarinorbert555@gmail.com>
2025-03-19 19:51:08 +0100

    Fixed the weird flashing of inputs and buttons on the login and register pages

--- frontend/nice-cards/src/app/app.config.ts
+++ frontend/nice-cards/src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
 
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -9,5 +10,8 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(
+      withFetch(),
+    ),
   ],
 };
--- frontend/nice-cards/src/app/shared/components/form-input/form-input.component.ts
+++ frontend/nice-cards/src/app/shared/components/form-input/form-input.component.ts
@@ -1,4 +1,4 @@
-import { NgClass } from '@angular/common';
+import { AsyncPipe, NgClass } from '@angular/common';
 import { Component, forwardRef, input, OnInit } from '@angular/core';
 import {
   ControlValueAccessor,
@@ -23,48 +23,50 @@ import {
     },
   ],
   template: `
-    <div class="relative">
-      <input
-        [id]="id()"
-        [name]="name()"
-        [type]="type()"
-        [required]="required()"
-        [placeholder]="placeholder()"
-        [formControl]="formControl"
-        class="peer w-full rounded-md border-skin-color-faintest bg-skin-color-surface text-skin-base placeholder-transparent shadow-sm placeholder:select-none focus:border-skin-color-primary focus:ring-0"
-        [ngClass]="{
-          'border border-red-500':
+    @if (formControl) {
+      <div class="relative">
+        <input
+          [id]="id()"
+          [name]="name()"
+          [type]="type()"
+          [required]="required()"
+          [placeholder]="placeholder()"
+          [formControl]="formControl"
+          class="peer w-full rounded-md border-skin-color-faintest bg-skin-color-surface text-skin-base placeholder-transparent shadow-sm placeholder:select-none focus:border-skin-color-primary focus:ring-0"
+          [ngClass]="{
+            'border required:border-red-500':
+              formControl.invalid &&
+              (formControl.dirty || formControl.touched) &&
+              formControl.errors?.['required'],
+          }"
+          [title]="
             formControl.invalid &&
             (formControl.dirty || formControl.touched) &&
-            formControl.errors?.['required'],
-        }"
-        [title]="
+            formControl.errors?.['required']
+              ? 'This field is required'
+              : ''
+          "
+        />
+        <label
+          [htmlFor]="name()"
+          class="pointer-events-none absolute -top-2.5 left-2 bg-skin-color-surface px-1 text-sm text-skin-faint transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:bg-transparent peer-placeholder-shown:text-base peer-focus:-top-2.5 peer-focus:left-2 peer-focus:bg-skin-color-surface peer-focus:text-sm"
+        >
+          {{ placeholder() }}
+        </label>
+
+        @if (
           formControl.invalid &&
           (formControl.dirty || formControl.touched) &&
-          formControl.errors?.['required']
-            ? 'This field is required'
-            : ''
-        "
-      />
-      <label
-        [htmlFor]="name()"
-        class="pointer-events-none absolute -top-2.5 left-2 bg-skin-color-surface px-1 text-sm text-skin-faint transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:bg-transparent peer-placeholder-shown:text-base peer-focus:-top-2.5 peer-focus:left-2 peer-focus:bg-skin-color-surface peer-focus:text-sm"
-      >
-        {{ placeholder() }}
-      </label>
-
-      @if (
-        formControl.invalid &&
-        (formControl.dirty || formControl.touched) &&
-        formControl.errors?.['email']
-      ) {
-        <div
-          class="absolute right-4 top-1/2 -translate-y-1/2 bg-skin-color-surface text-sm text-skin-muted"
-        >
-          <span>Invalid email format</span>
-        </div>
-      }
-    </div>
+          formControl.errors?.['email']
+        ) {
+          <div
+            class="absolute right-4 top-1/2 -translate-y-1/2 bg-skin-color-surface text-sm text-skin-muted"
+          >
+            <span>Invalid email format</span>
+          </div>
+        }
+      </div>
+    }
   `,
 })
 export class FormInputComponent implements OnInit, ControlValueAccessor {
